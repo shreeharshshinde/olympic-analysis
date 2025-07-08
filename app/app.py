fb90f8fc10ea53b080316929a99c3ff5d4cf2abf
Shreeharsh Shinde <shindeshreeharsh157@gmail.com>
2025-07-08 21:32:12 +0530

    Medal Tally Done

---
+++ app/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import preprocessor
+import helper
+
+st.set_page_config(layout="wide")
+
+df = pd.read_csv('data/athlete_events.csv')
+region_df = pd.read_csv('data/noc_regions.csv')
+
+df = preprocessor.preprocess(df, region_df)
+
+st.sidebar.title("Olympics Analysis")
+
+user_menu = st.sidebar.radio(
+    "Select an Option",
+    ("Medal Tally", "Overall Analysis", "Country-wise Analysis", "Athlete-wise Analysis")
+)
+
+if user_menu == 'Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years, country = helper.country_year_list(df)
+    selected_year = st.sidebar.selectbox("Select Year", years)
+    selected_country = st.sidebar.selectbox("Select Country", country)
+    medal_tally = helper.medal_tally(df)
+    st.dataframe(medal_tally)
+
+
