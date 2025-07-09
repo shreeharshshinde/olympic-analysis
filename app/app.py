e5dfc3469cbfdc4d0ca2eb9b5c5d18b3a236cd11
Shreeharsh Shinde <shindeshreeharsh157@gmail.com>
2025-07-09 16:37:06 +0530

    overall analysis work

--- app/app.py
+++ app/app.py
@@ -34,3 +34,33 @@ if user_menu == 'Medal Tally':
     st.table(medal_tally)
 
 
+if user_menu == "Overall Analysis":
+    editions = df['Year'].unique().shape[0]
+    cities = df['City'].unique().shape[0]
+    sports = df['Sport'].unique().shape[0]
+    events = df['Event'].unique().shape[0]
+    athletes = df['Name'].unique().shape[0]
+    nations = df['region'].unique().shape[0]
+
+    st.title("Top Statistics")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Editions")
+        st.header(editions)
+    with col2:
+        st.header("Hosts")
+        st.header(cities)
+    with col3:
+        st.header("Sports")
+        st.header(sports)
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Events")
+        st.header(events)
+    with col2:
+        st.header("Athletes")
+        st.header(athletes)
+    with col3:
+        st.header("Nations")
+        st.header(nations)
